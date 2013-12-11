0c1cd98f22c801126a3989f5a7b367a545d6b562
= <mike@madebymany.co.uk>
2013-12-11 20:37:46 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MikelaurieBlog::Application.config.secret_key_base = '28ef367308a90648db700a669677dee572c53d4d15cea99b1332261ce5dae807041f0e88b592dfce80144fbe27b609404ab1a7a9d8e632aeb56f746da3ab1c7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MikelaurieBlog::Application.config.session_store :cookie_store, key: '_mikelaurie_blog_session'
